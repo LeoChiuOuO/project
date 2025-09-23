9700ab2dd74e112403316b93b65a790c521224c4
LeoChiuOuO <leochiuouo@gmail.com>
2025-09-23 09:04:05 +0800

    V1.0_DEMO版

--- obj/Debug/net9.0/WebApplication_Dianthus.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication_Dianthus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication_Dianthus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ce5e2b9a1e93cd27adb28a1ecfad41444d1ab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85fa7fbfd284b8e51e6b1c92233b2e0de0b318ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication_Dianthus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication_Dianthus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
