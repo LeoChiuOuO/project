5aee569e88f511f137777c66ccc681c9d8ef8bf1
LeoChiuOuO <leochiuouo@gmail.com>
2025-11-24 10:32:58 +0800

    修正homeview_index的重大div顏色

--- obj/Release/net9.0/WebApplication_Dianthus.AssemblyInfo.cs
+++ obj/Release/net9.0/WebApplication_Dianthus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication_Dianthus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0916c67d9cd38c90ef0f89a92e63d648fef05a4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46cab13a2bdfad4fddbe879d1c1c579a770eea70")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication_Dianthus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication_Dianthus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
